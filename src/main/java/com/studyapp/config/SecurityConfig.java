395c6d7e6230a6cd320818b00a490f95330bf2d8
KimHoGyun <khg010122@naver.com>
2025-08-16 08:26:23 +0900

    beckend commit

--- src/main/java/com/studyapp/config/SecurityConfig.java
+++ src/main/java/com/studyapp/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList(
                 "http://localhost:3000",
-                " https://studyplanner-95285.web.app" // <-- 새로 발급받은 주소 추가
+                "https://studyplanner-95285.web.app" // <-- 새로 발급받은 주소 추가
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
